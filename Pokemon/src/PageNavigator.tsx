e2d84891dbac72962852854688f49b33a9fc11c5
Suzy-Lee <mulddang2@naver.com>
2023-08-22 21:37:54 +0900

    Feat: add Page Navigator
    react-router-dom

---
+++ Pokemon/src/PageNavigator.tsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import PokeCardList from "./List/PokeCardList";
+import PokemonDetail from "./Detail/PokemonDetail";
+
+
+const PageNavigator = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<PokeCardList />}/>
+      <Route path="/pokemon/:name" element={<PokemonDetail />}/>
+    </Routes>
+  )
+}
+
+export default PageNavigator
