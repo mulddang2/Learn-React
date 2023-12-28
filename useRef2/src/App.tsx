920925f355b10418ef6b5416e22e564aaa256414
Suzy-Lee <>
2023-12-28 20:18:01 +0900

    study: 배열에 항목 추가하기
    배열에 항목 추가할 때, spread 연산자 or concat 메서드 사용하기

---
+++ useRef2/src/App.tsx
@@ -0,0 +1,69 @@
+import { useRef, useState } from 'react';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
+
+function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+
+  const { username, email } = inputs;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+    },
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    // NOTE: 방법 1. 원본 복사해서 변경 setUsers([...users, user]);
+    // NOTE 방법 2. 원본 유지하는 concat 메서드 사용
+    setUsers(users.concat(user))
+
+    setInputs({
+      username: '',
+      email: '',
+    });
+    nextId.current += 1;
+  };
+
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
+    </>
+  );
+}
+
+export default App;
