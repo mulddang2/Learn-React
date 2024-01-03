713677853662d30071b2f36b3358f5fc078c8543
Suzy-Lee <>
2024-01-03 17:22:00 +0900

    study: useCallback
    1. useCallback 왜 사용하나?
    - props가 바뀌지 않았으면 virtual DOM에 새로 렌더링 하는 것 조차 안하고 싶어서
    2. 사용법:
    - useCallback(함수, [함수안에 쓰는 모든 상태])

--- useMemo/src/App.tsx
+++ useMemo/src/App.tsx
@@ -1,91 +1,105 @@
-import { useMemo, useRef, useState } from 'react';
-import CreateUser from './CreateUser';
+import React, { useRef, useState, useMemo, useCallback } from 'react';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
-function counterActiveUsers(users) {
-  console.log('활성 사용자 수를 세는 중...');
-
-  return users.filter((user) => user.active).length;
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는중...');
+  return users.filter(user => user.active).length;
 }
+
+/**
+ * NOTE: 
+ * 1. useCallback 왜 사용하나?
+ * - props가 바뀌지 않았으면 virtual DOM에 새로 렌더링 하는 것 조차 안하고 싶어서
+ * 2. 사용법:
+ * - useCallback(함수, [함수안에 쓰는 모든 상태])
+ */
+
 
 function App() {
   const [inputs, setInputs] = useState({
     username: '',
-    email: '',
+    email: ''
   });
   const { username, email } = inputs;
-  const onChange = (e) => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
-  };
-
+  const onChange = useCallback(
+    e => {
+      const { name, value } = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value
+      });
+    },
+    [inputs]
+  );
   const [users, setUsers] = useState([
     {
       id: 1,
-      username: 'dodo1',
-      email: 'dodo1@gamil.com',
-      active: true,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: true
     },
     {
       id: 2,
-      username: 'dodo2',
-      email: 'dodo2@gamil.com',
-      active: false,
+      username: 'tester',
+      email: 'tester@example.com',
+      active: false
     },
     {
       id: 3,
-      username: 'dodo3',
-      email: 'dodo3@gamil.com',
-      active: false,
-    },
+      username: 'liz',
+      email: 'liz@example.com',
+      active: false
+    }
   ]);
 
   const nextId = useRef(4);
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
-      email,
+      email
     };
     setUsers(users.concat(user));
 
     setInputs({
       username: '',
-      email: '',
+      email: ''
     });
     nextId.current += 1;
-  };
+  }, [users, username, email]);
 
-  const onRemove = (id) => {
-    setUsers(users.filter((user) => user.id !== id));
-  };
-
-  const onToggle = (id) => {
-    setUsers(
-      users.map((user) =>
-        user.id == id ? { ...user, active: !user.active } : user
-      )
-    );
-  };
-
-  /*
-  NOTE: 
-  1. useMemo 왜 사용? ==> 필요한 값 이외 input의 내용을 바꿀 때도 리렌더링 되어, 성능최적화 하려고 사용함 
-  2. useMemo(함수 , [렌더링 필요한 배열])
-  : 렌더링 필요한 배열 안에 내용이 바뀌면, 함수를 호출해서 값을 연산해주고, 내용이 바뀌지 않았다면, 이전에 연산한 값을 사용한다
-  */
-  const count = useMemo(()=>counterActiveUsers(users), [users])
-
+  const onRemove = useCallback(
+    id => {
+      // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+      // = user.id 가 id 인 것을 제거함
+      setUsers(users.filter(user => user.id !== id));
+    },
+    [users]
+  );
+  const onToggle = useCallback(
+    id => {
+      setUsers(
+        users.map(user =>
+          user.id === id ? { ...user, active: !user.active } : user
+        )
+      );
+    },
+    [users]
+  );
+  const count = useMemo(() => countActiveUsers(users), [users]);
   return (
     <>
-      <CreateUser username={username} email={email} onCreate={onCreate} onChange={onChange} />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
-      <div>활성 사용자 수: {count}</div>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성사용자 수 : {count}</div>
     </>
-  )
+  );
 }
 
 export default App;
