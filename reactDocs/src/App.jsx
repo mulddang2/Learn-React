014b2eda8e5bd09cdb9a071e294c5cb90b328a71
Suzy-Lee <>
2024-01-18 23:59:39 +0900

    study: JSX에서 중괄호를 이용하여 JavaScript 사용하기

--- reactDocs/src/App.jsx
+++ reactDocs/src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css';
+const person = {
+  name: '스펀지Bob',
+  theme: {
+    backgroundColor: 'skyblue',
+    color: 'magenta'
+  }
 
+}
 function TodoList() {
   return (
-    <section>
-      <h1>스펀지밥의 할일</h1>
+    <section style={person.theme}>
+      {/* NOTE: 홀따옴표(&apos;)를  엔티티로 안하면 오류가 난다. */}
+      <h1>{person.name}&apos;s Todos</h1>
       <img src='https://mblogthumb-phinf.pstatic.net/MjAyMDA3MDdfMTcx/MDAxNTk0MDc1MjA4MTk2.Aekb3OsqyiALI0sM19gUybs0yezPZjNU1hqCGN50oXUg.JW3mTukm7Yn5MBXIUJMXvMMjfmu0iGjQqDeHXwEkGCUg.GIF.car5464/IMG_0226.GIF?type=w800' alt='스펀지밥' className="photo"/>
       <ul>
         <li>끝내주게 숨쉬기</li>
