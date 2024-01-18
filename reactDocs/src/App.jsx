2f39c8e2d830ae40bf0b0f1f6646adf9382ee43b
Suzy-Lee <>
2024-01-18 23:48:22 +0900

    study: JSX로 마크업 작성하기

--- reactDocs/src/App.jsx
+++ reactDocs/src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css';
-import Profile from './describing-the-UI/Profile';
 
-function Gallery() {
+function TodoList() {
   return (
     <section>
-      <h1>I like it</h1>
-      <Profile />
-      <Profile />
-      <Profile />
+      <h1>스펀지밥의 할일</h1>
+      <img src='https://mblogthumb-phinf.pstatic.net/MjAyMDA3MDdfMTcx/MDAxNTk0MDc1MjA4MTk2.Aekb3OsqyiALI0sM19gUybs0yezPZjNU1hqCGN50oXUg.JW3mTukm7Yn5MBXIUJMXvMMjfmu0iGjQqDeHXwEkGCUg.GIF.car5464/IMG_0226.GIF?type=w800' alt='스펀지밥' className="photo"/>
+      <ul>
+        <li>끝내주게 숨쉬기</li>
+        <li>간지나게 자기</li>
+        <li>작살나게 밥먹기</li>
+      </ul>
     </section>
   );
 }
 
-export default Gallery;
+export default TodoList;
