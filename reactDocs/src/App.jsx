d52f1ccbc4171921e60138e23e6ab78b0f94d2a7
Suzy-Lee <>
2024-01-18 23:33:07 +0900

    study: 첫 컴포넌트 만들기

--- reactDocs/src/App.jsx
+++ reactDocs/src/App.jsx
@@ -1,21 +1,33 @@
 import './App.css';
-import { useState } from "react"
 
-import MyButton from './responding-to-events/MyButton';
-
-function App() {
-  const [count, setCount] = useState(0)
-  const handleClick = () => {
-    setCount(count + 1)
-  }
+function Profile() {
+  const user = {
+    name: 'kuromi',
+    imageUrl:
+      'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/90f9af29-a2ea-4c71-be11-6489816dae27/dfrfnz8-8a9d60bf-88c5-4260-a2f4-b97709058180.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzkwZjlhZjI5LWEyZWEtNGM3MS1iZTExLTY0ODk4MTZkYWUyN1wvZGZyZm56OC04YTlkNjBiZi04OGM1LTQyNjAtYTJmNC1iOTc3MDkwNTgxODAuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.LPU9hES5y0qKE7gQ4EWIyjoOCUQUQQXe4WnHj0FZNdg',
+    imageSize: 90,
+  };
   return (
-  <>
-    <h1>Counters that update separately</h1>
-    <div><MyButton count={count} onClick={handleClick}/></div>
-    <div><MyButton count={count} onClick={handleClick}/></div>
-  </>
-  )
-  ;
+    <img
+      src={user.imageUrl}
+      alt={user.name}
+      style={{
+        width: user.imageSize,
+        height: user.imageSize,
+      }}
+    />
+  );
 }
 
-export default App;
+function Gallery() {
+  return (
+    <section>
+      <h1>I like it</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
+
+export default Gallery;
