252ec3290c57260103805324fbc8310061d24b8a
Suzy-Lee <>
2024-01-19 17:08:04 +0900

    study: 안순수한 컴포넌트 순수하게 만들기
    기존 변수를 수정하는 대신 {prop}을 전달하여 컴포넌트를 순수하게 한다.

--- reactDocs/src/App.jsx
+++ reactDocs/src/App.jsx
@@ -1,23 +1,37 @@
-import './App.css'
-import { people } from './rendering-lists/data';
-import { getImageUrl } from './rendering-lists/util';
+// 이렇게 만들면 안순수하다. 변수의 값이 예측불가 하기 때문에,,
+/* 이렇게 하면 안된다..
+let guest = 0;
 
-export default function List() {
-  const listItem = people.map((person) => (
-    <li key={person.id}>
-      <img src={getImageUrl(person)} className='list_img' alt={person.name} />
-      <p>
-        <b>{person.name}:</b> {person.profession} known for{' '}
-        {person.accomplishment}
-      </p>
-    </li>
-  ));
+function Cup() {
+  // 전역에 냅둔 기존 변수를 변경합니다.
+  guest += 1;
+  return <h2>손님이 시킨 음료 갯수: #{guest}</h2>
+}
+
+export default function CupSet() {
+  console.log(guest)
   return (
-    <section>
-      <h1>Scientists</h1>
-      <ul className='people'>
-        {listItem}
-      </ul>
-    </section>
-  );
+    <>
+      <Cup />{console.log(guest)}
+      <Cup />{console.log(guest)}
+      <Cup />{console.log(guest)}
+      <Cup />{console.log(guest)}
+      <Cup />{console.log(guest)}
+    </>
+  )
+}
+*/
+
+function Cup({guest}) {
+  return <h2>손님이 시킨 음료 갯수: #{guest}</h2>
+}
+
+export default function CupSet() {
+  return (
+    <>
+      <Cup guest={1}/>
+      <Cup guest={2}/>
+      <Cup guest={3}/>
+    </>
+  )
 }
